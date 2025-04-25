c8627e3e30012dea88c4e5d38e3b7a90ca1b2003
Aidan Cormier <alwc935e@gmail.com>
2025-04-25 10:14:45 -0500

    Prep-for-prod

--- SADjango/settings.py
+++ SADjango/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6v_l1g=k8bj*5i7v75+@skk*&o$i(lx1rr3e#eu*k#9k&glsc'
+SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '3.132.251.228']
 
 
 # Application definition
