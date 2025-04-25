0472c6bc4f2904338f157ae6244e723f5dc53634
Aidan Cormier <alwc935e@gmail.com>
2025-04-25 11:10:13 -0500

    updating allowed hosts

--- SADjango/settings.py
+++ SADjango/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = ['127.0.0.1', '3.132.251.228']
+ALLOWED_HOSTS = ['127.0.0.1', '3.132.251.228', 'sanderson-db.com']
 
 
 # Application definition
